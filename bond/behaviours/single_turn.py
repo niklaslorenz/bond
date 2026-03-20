511fe04f482a91e433d1d4cadcaa73d14fbda4ba
Niklas Lorenz <niklaslorenz1999@gmail.com>
2026-03-20 15:35:10 +0100

    implemented the single_turn behaviour

--- bond/behaviours/single_turn.py
+++ bond/behaviours/single_turn.py
@@ -2,11 +2,11 @@ import logging
 
 from returns.result import Success
 
-from bond.conversation import Conversation
-from bond.endpoints.chat_completions import (AssistantMessage,
-                                             ChatCompletionsProvider,
-                                             ChatCompletionsWrapper,
-                                             FunctionCall, TextChunk)
+from bond.bond_environment import BondEnvironment
+from bond.conversation import Conversation, ConversationMessage
+from bond.endpoints.chat_completions import (AssistantMessage, FunctionCall,
+                                             SystemMessage, TextChunk)
+from bond.providers.provider import build_toolbox
 from bond.tools.tool import Toolbox
 
 logger = logging.getLogger(__name__)
@@ -24,31 +24,58 @@ def _do_tool_call(toolbox: Toolbox, function_call: FunctionCall) -> str:
 class SingleTurn:
     def __init__(
         self,
-        provider: ChatCompletionsProvider,
-        model: str,
-        toolbox: Toolbox,
-        system_message: str | None = None,
-        **additional_model_arguments,
+        environment: BondEnvironment,
+        persona: str,
+        user_name: str = "user",
+        **additional_chat_completion_arguments,
     ):
-        self.toolbox = toolbox
-        self.system_message = system_message
-        self.chat_completions = ChatCompletionsWrapper(provider)
-        self.model = model
-        self.additional_model_arguments = additional_model_arguments
+        self.environment = environment
+        self.persona = environment.get_persona(persona)
+        self.user_name = user_name
+        self.model = self.persona.model
+        self.provider = self.environment.get_provider(self.persona.provider)
+        self.system_message = (
+            SystemMessage.create(self.persona.system_prompt)
+            if self.persona.system_prompt is not None
+            else None
+        )
+        self.additional_model_arguments = additional_chat_completion_arguments
 
-    def run(self, user_message: str) -> str:
-        conversation = Conversation.create(self.system_message)
-        conversation.add_user_message(user_message)
+    def run(self, user_message: str) -> tuple[str, Conversation]:
+        toolbox = build_toolbox(
+            self.provider,
+            [
+                tool
+                for toolset in self.persona.toolbox
+                for tool in self.environment.get_toolset(toolset)
+            ],
+        )
+        conversation = Conversation.create(self.persona.name, self.user_name)
+        if self.system_message is not None:
+            conversation.add_message(
+                ConversationMessage(author="System", message=self.system_message)
+            )
+        conversation.add_message(
+            ConversationMessage.create_user_message(user_message, self.user_name)
+        )
 
         response_acc: list[str] = []
         while True:
-            response = self.chat_completions.chat_completion(
+            response = self.provider.chat_completions().chat_completion(
                 self.model,
-                conversation.get_messages(),
-                tools=self.toolbox.get_tool_descriptions(),
+                conversation.get_chat_completion_messages(),
+                tools=toolbox.get_tool_descriptions(),
                 **self.additional_model_arguments,
             )
             message = response.choices[0].message
+            conversation.add_message(
+                ConversationMessage(
+                    author=self.persona.name,
+                    message=AssistantMessage(
+                        content=message.content, tool_calls=message.tool_calls
+                    ),
+                )
+            )
 
             # Add text content to the output
             if message.content is not None:
@@ -58,12 +85,11 @@ class SingleTurn:
 
             # Return when no tools are called
             if message.tool_calls is None:
-                return "\n".join(response_acc).strip("\n ")
+                return "\n".join(response_acc).strip("\n "), conversation
 
             # Handle Tool calls
-            conversation.add_message(
-                AssistantMessage(content=message.content, tool_calls=message.tool_calls)
-            )
             for tool_call in message.tool_calls:
-                result = _do_tool_call(self.toolbox, tool_call.function)
-                conversation.add_tool_str_response(tool_call, result)
+                result = _do_tool_call(toolbox, tool_call.function)
+                conversation.add_message(
+                    ConversationMessage.create_tool_response_message(result, tool_call)
+                )
