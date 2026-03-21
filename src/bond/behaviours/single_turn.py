7f498c18d9194fe0a2aa6d3910bd4db8a0955944
Niklas Lorenz <niklaslorenz1999@gmail.com>
2026-03-21 20:52:28 +0100

    Adjusted single_turn and ask to work with the new environments

--- src/bond/behaviours/single_turn.py
+++ src/bond/behaviours/single_turn.py
@@ -4,15 +4,13 @@ from returns.result import Success
 
 from bond.bond_environment import BondEnvironment
 from bond.conversation import Conversation, ConversationMessage
-from bond.endpoints.chat_completions import (
-    AssistantMessage,
-    FunctionCall,
-    SystemMessage,
-    TextChunk,
-)
+from bond.endpoints.chat_completions import (AssistantMessage, FunctionCall,
+                                             SystemMessage, TextChunk,
+                                             ThinkChunk)
+from bond.io.io_env import IOEnvironment
 from bond.providers.provider import build_toolbox
 from bond.tools.shell import allow_shell_commands
-from bond.tools.tool import Toolbox
+from bond.tools.tool import Toolbox, ToolEnvironment
 
 logger = logging.getLogger(__name__)
 
@@ -32,13 +30,19 @@ class SingleTurn:
         environment: BondEnvironment,
         persona: str,
         user_name: str = "user",
+        io_environment: IOEnvironment | None = None,
+        tool_environment: ToolEnvironment | None = None,
         allow_shell_executions: bool = False,
         **additional_chat_completion_arguments,
     ):
         self.environment = environment
         self.persona = environment.get_persona(persona)
         self.user_name = user_name
+        self.io_environment = io_environment or IOEnvironment(None, None, None)
+        self.tool_environment = tool_environment or ToolEnvironment()
         self.allow_shell_executions = allow_shell_executions
+        self.additional_model_arguments = additional_chat_completion_arguments
+
         self.model = self.persona.model
         self.provider = self.environment.get_provider(self.persona.provider)
         self.system_message = (
@@ -46,10 +50,7 @@ class SingleTurn:
             if self.persona.system_prompt is not None
             else None
         )
-        self.additional_model_arguments = additional_chat_completion_arguments
-
-    def run(self, user_message: str) -> tuple[str, Conversation]:
-        toolbox = build_toolbox(
+        self.toolbox = build_toolbox(
             self.provider,
             [
                 tool
@@ -57,6 +58,8 @@ class SingleTurn:
                 for tool in self.environment.get_toolset(toolset)
             ],
         )
+
+    def run(self, user_message: str) -> Conversation:
         conversation = Conversation.create(self.persona.name, self.user_name)
         if self.system_message is not None:
             conversation.add_message(
@@ -66,12 +69,11 @@ class SingleTurn:
             ConversationMessage.create_user_message(user_message, self.user_name)
         )
 
-        response_acc: list[str] = []
         while True:
             response = self.provider.chat_completions().chat_completion(
                 self.model,
                 conversation.get_chat_completion_messages(),
-                tools=toolbox.get_tool_descriptions(),
+                tools=self.toolbox.get_tool_descriptions(),
                 **self.additional_model_arguments,
             )
             message = response.choices[0].message
@@ -88,19 +90,30 @@ class SingleTurn:
             if message.content is not None:
                 for chunk in message.content:
                     if isinstance(chunk, TextChunk):
-                        response_acc.append(chunk.text)
+                        if self.io_environment.text_out is not None:
+                            self.io_environment.text_out.write(chunk.text + "\n")
+                    if isinstance(chunk, ThinkChunk):
+                        if self.io_environment.thought_out is not None:
+                            for think_chunk in chunk.thinking:
+                                if isinstance(think_chunk, TextChunk):
+                                    self.io_environment.thought_out.write(
+                                        think_chunk.text + "\n"
+                                    )
 
             # Return when no tools are called
             if message.tool_calls is None:
-                return "\n".join(response_acc).strip("\n "), conversation
+                return conversation
 
             # Handle Tool calls
             for tool_call in message.tool_calls:
-                if self.allow_shell_executions:
-                    with allow_shell_commands():
-                        result = _do_tool_call(toolbox, tool_call.function)
-                else:
-                    result = _do_tool_call(toolbox, tool_call.function)
-                conversation.add_message(
-                    ConversationMessage.create_tool_response_message(result, tool_call)
-                )
+                with self.tool_environment.activate():
+                    if self.allow_shell_executions:
+                        with allow_shell_commands():
+                            result = _do_tool_call(self.toolbox, tool_call.function)
+                    else:
+                        result = _do_tool_call(self.toolbox, tool_call.function)
+                    conversation.add_message(
+                        ConversationMessage.create_tool_response_message(
+                            result, tool_call
+                        )
+                    )
--- src/bond/cli/ask.py
+++ src/bond/cli/ask.py
@@ -1,32 +1,47 @@
+import sys
 from argparse import ArgumentParser
 from pathlib import Path
 
 from bond.behaviours.single_turn import SingleTurn
 from bond.bond_environment import DynamicBondEnvironment
 from bond.config import BondConfig
+from bond.io.io_env import IOEnvironment
 from bond.tools import global_toolbox, tool
 
 
 def main():
+
+    # Parse args
     parser = ArgumentParser("Ask Bond")
     parser.add_argument("first", type=str)
     parser.add_argument("second", type=str, nargs="?")
+    parser.add_argument("--show-thoughts", action="store_true")
+    parser.add_argument("--non-interactive", action="store_true")
 
     args = parser.parse_args()
+    request = args.first if args.second is None else args.second
 
+    # Setup environment
     env_path = Path("~/.config/bond").expanduser().absolute()
     config_path = env_path / "config.json"
-
     config = BondConfig.load_from(config_path)
     env = DynamicBondEnvironment(
-        env_path,
-        {
-            k: v
-            for k, v in global_toolbox.build_global_toolsets().items()
-            if k in config.ask.tools
-        },
+        env_path, global_toolbox.get_toolsets(config.ask.tools)
+    )
+    tool_environment = tool.ToolEnvironment(
+        interaction_io=(
+            tool.BidirectionalTextIO(text_in=sys.stdin, text_out=sys.stdout)
+            if not args.non_interactive
+            else None
+        )
+    )
+    io_environment = IOEnvironment(
+        text_in=sys.stdin,
+        text_out=sys.stdout,
+        thought_out=sys.stdout if args.show_thoughts else None,
     )
 
+    # Get persona
     persona: str = (
         config.ask.get_default_persona() if args.second is None else args.first
     )
@@ -34,14 +49,17 @@ def main():
         raise ValueError(
             f"not a valid persona: {persona}. Available personas: {'\n'.join(config.ask.personas)}"
         )
-    request = args.first if args.second is None else args.second
-
-    tool.set_interactive(True)
     allow_shell = "shell" in env.get_persona(persona).toolbox
 
-    turn = SingleTurn(env, persona, allow_shell_executions=allow_shell)
-    result, conversation = turn.run(request)
-    print(result)
+    turn = SingleTurn(
+        env,
+        persona,
+        tool_environment=tool_environment,
+        io_environment=io_environment,
+        allow_shell_executions=allow_shell,
+    )
+    conversation = turn.run(request)
+    # TODO: save conversation as last-ask so the user can followup
 
 
 if __name__ == "__main__":
