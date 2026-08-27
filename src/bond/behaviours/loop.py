766c596d781940d884d42440128038624eed44d4
Niklas Lorenz <niklaslorenz1999@gmail.com>
2026-08-27 22:33:52 +0200

    Implemented save after turn (which is the default unless disabled with a flag)

--- src/bond/behaviours/loop.py
+++ src/bond/behaviours/loop.py
@@ -38,7 +38,8 @@ class LoopBehaviour:
         allow_shell_executions: bool = False,
         user_name: str | None = None,
         allowed_personas: list[str] | None = None,
-        max_retries: int = 10,
+        save_after_turn: bool = False,
+        **additional_chat_completion_arguments,
     ):
         self.runtime = runtime
         self.conversation = conversation
@@ -52,7 +53,8 @@ class LoopBehaviour:
         self.allow_shell_executions = allow_shell_executions
         self.user_name = user_name
         self.allowed_personas = allowed_personas
-        self.max_retries = max_retries
+        self.save_after_turn = save_after_turn
+        self.additional_chat_completion_arguments = additional_chat_completion_arguments
 
         self.running = False
         self.persona: Persona
@@ -165,6 +167,12 @@ class LoopBehaviour:
                 )
                 try:
                     self.turn.run(self.conversation)
+                    if self.save_after_turn:
+                        if self.conversation.name is not None:
+                            conv_path = self.runtime.get_data_dir()
+                            self.conversation.save_to_file(
+                                conv_path / self.conversation.name
+                            )
                 except Exception as e:
                     self.event_handler(ErrorEvent(error=e, critical=False))
             else:
